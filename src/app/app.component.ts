4c8435d67c15d08c15fa8643dbd1239a94a71d83
Sumair Syed <sumair_ahmed4@yahoo.co.in>
2022-02-19 14:59:37 +1100

    Updated styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'winning-group';
+
+  currentRoute: string;
+
+  constructor(private router: Router) {}
+
+  public ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    ).subscribe(event => {
+      console.log(event);
+      this.currentRoute = (event as any).url;
+      console.log(this.currentRoute);
+    });
+  }
 }
