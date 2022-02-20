f4475cedf72ed05d95b2cd6789ec9dc8b567ffa8
Sumair Syed <sumair_ahmed4@yahoo.co.in>
2022-02-20 23:32:32 +1100

    Added default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ErrorsComponent } from './errors/errors.component';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'shopping-cart', component: ShoppingCartComponent },
